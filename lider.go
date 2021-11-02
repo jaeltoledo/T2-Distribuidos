24f89ded231d4bf6ad04f647ff6c344706cc2eeb
CDonosoK <clemente.donosok@gmail.com>
2021-11-02 19:20:43 -0300

    archivos iniciales

---
+++ lider.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+)
+
+func iniciarJuego(){
+
+}
+
+func enviarJugadas(){
+
+}
+
+func jugadaJugador(){
+
+}
+
+func etapa1(){
+
+}
+
+func etapa2(){
+	
+}
+
+func etapa3(){
+	
+}
+
+func main(){
+
+}
