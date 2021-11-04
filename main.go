9548b7d9ea980003c2ee32d3ace768cba6938563
nazadt <nazareth.diaztapia@gmail.com>
2021-11-04 02:27:47 -0300

    pozo v1

---
+++ main.go
@@ -0,0 +1,55 @@
+//Lider para la conexion del pozo
+package main
+
+import (
+	"log"
+	"strconv"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+// Envia mensaje al pozo con el jugador eliminado.
+func jugador_eliminado(id int, ronda int) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	jug := strconv.Itoa(id)
+	ron := strconv.Itoa(ronda)
+	body := jug + "-" + ron
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
+
+func main() {
+	jugador_eliminado(10, 2)
+	return
+}
